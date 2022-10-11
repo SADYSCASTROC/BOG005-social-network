9313e7b8b1bc23d5966389a2098ab7e91f8ade19
SADYSCASTROC <sadyscastro@gmail.com>
2022-10-11 10:35:55 -0500

    agregando likes y deslikes

--- src/components/wall.js
+++ src/components/wall.js
@@ -1,113 +1,38 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-tabs */
 import {
-  savePost, onGetPost, deletePost, getPostOne, updatePost, logOut, currentUser, auth, likePost, DeletelikePost,
+  savePost, onGetPost, deletePost, getPostOne, updatePost, logOut, auth, likePost, DeletelikePost,
 } from '../lib/firebase/firebaseService.js';
 
- HEAD
-import { savePost, getPost, onGetPost, deletePost, getPostOne, updatePost, } from "../lib/firebase/firebaseService.js";
 export const wall = () => {
-	const sectionWall = document.createElement('section');
-	sectionWall.className = 'sectionWall';
-          sectionWall.innerHTML = `
+  const sectionWall = document.createElement('section');
+  sectionWall.className = 'sectionWall';
+  sectionWall.innerHTML = `
 	<header class=headerWall>
 	   <img class="imgLogoWall" src="IMG/Explore-removebg-preview.png">
-          </header>
+  </header>
+  <button class="wallLogout">salir</button>
 	<section>
 		<form class="formWall">
 			<input type="text" placeholder="Description" id="post">
 			<button type="submit" id="btnPost">Post</button>
 		</form>
-		<div id="createPost"></div>
+		<texarea id="createPost"></texarea>
 	</section>
-  `
-
-	const formPost = sectionWall.querySelector('.formWall');
-	const descriptionPost = sectionWall.querySelector('#post');
-	const postContainer = sectionWall.querySelector('#createPost');
-	let editStatus = false;
-	let id = '';
-
- const wall = () => {
-  const sectionWall = document.createElement('section');
-  sectionWall.className = 'sectionWall';
-  sectionWall.innerHTML = `<h1 class="tituloo">este es mi muro</h1>
-  <button class="wallLogout">salir</button>
-  <form class="formWall">   
-    <input type="text" placeholder="Description" id="post">
-
-    <button type="submit" id="btnPost">Post</button>
-  </form>
-  <div id="createPost"></div>
   `;
 
   const formPost = sectionWall.querySelector('.formWall');
   const descriptionPost = sectionWall.querySelector('#post');
   const postContainer = sectionWall.querySelector('#createPost');
   const logOutButton = sectionWall.querySelector('.wallLogout');
-
   let editStatus = false;
   let id = '';
-  const like = [];
 
-
-	formPost.addEventListener('submit', (e) => {
-		e.preventDefault();
-
-
-		if (!editStatus) {
-			savePost(descriptionPost.value);
-		} else {
-			updatePost(id, {
-				description: descriptionPost.value,
-			});
-			editStatus = false;
-		}
-		formPost.reset();
-
-	});
-
-	window.addEventListener('DOMContentLoaded', async () => {
-		onGetPost((querySnapshot) => {
-
-			let html = ''
-			querySnapshot.forEach(doc => {
-				const post = doc.data();
-				html += `
-                                               <div>
-                                                    <p> ${post.description}</p>
-                                               </div>
-				       <section clas="wallButtons">
-				            <a href="#" class="deleteButton" data-id="${doc.id}"><span class= icon-cross></span>Delete</a>
-                                                    <button class="editButton"  data-id="${doc.id}">Edit</button>
-				       </section>
-                                        `;
-			});
-			postContainer.innerHTML = html;
-
-			const buttonDelete = postContainer.querySelectorAll('.deleteButton')
-			buttonDelete.forEach(btn => {
-				btn.addEventListener('click', ({ target: { dataset } }) => {
-					deletePost(dataset.id);
-				});
-			});
-
-			const buttonEdit = postContainer.querySelectorAll('.editButton');
-
-			buttonEdit.forEach(btn => {
-				btn.addEventListener('click', async (e) => {
-					const doc = await getPostOne(e.target.dataset.id);
-					const postOne = doc.data();
-					formPost['post'].value = postOne.description;
-					editStatus = true;
-					id = e.target.dataset.id;
-					formPost['btnPost'].innerText = 'Update'
-				})
-			})
-
-		});
-	});
+  formPost.addEventListener('submit', (e) => {
+    e.preventDefault();
 
     if (!editStatus) {
-      savePost(descriptionPost.value, like);
+      savePost(descriptionPost.value);
     } else {
       updatePost(id, {
         description: descriptionPost.value,
@@ -115,32 +40,49 @@ export const wall = () => {
       editStatus = false;
     }
     formPost.reset();
-  };
+  });
 
   window.addEventListener('DOMContentLoaded', async () => {
     onGetPost((querySnapshot) => {
-      let html = '';
+      let htmladd = '';
+      // const htmlremove = '';
+
+      let p1 = '';
+
       querySnapshot.forEach((doc) => {
         const post = doc.data();
-        html += `
-       <div>
-       <p> ${post.email}</p>
 
-        <p> ${post.description}</p>
-       </div>
-       <button class="likeButton"  data-id="${doc.id}">like</button>
-       <button class="deleteButton"  data-id="${doc.id}">Delete</button>
-       <button class="editButton"  data-id="${doc.id}">Edit</button>
-      `;
-      });
-      postContainer.innerHTML = html;
-
-      const buttonDelete = postContainer.querySelectorAll('.deleteButton');
-      buttonDelete.forEach((btn) => {
-        btn.addEventListener('click', ({ target: { dataset } }) => {
-          deletePost(dataset.id);
+        let aux = 0;
+        p1 = '';
+      
+        post.like.map((index) => {
+          if (index === auth.currentUser.uid) {
+            aux = 1;
+            console.log('me encontraron');
+          }
         });
+
+        if (aux === 1) {
+          p1 += `<button class="RemovelikeButton"  data-id="${doc.id}">ya no me gusta</button>`;
+        } else {
+          p1 += `<button class="likeButton"  data-id="${doc.id}">me gusta</button>`;
+        }
+
+        htmladd += `
+          <div>
+          <p>  ${post.email} <br> <br> ${post.description}</p>
+          <p> ${post.like.length}</p>
+
+          <section clas="wallButtons">
+                    ${p1}
+                    <button class="deleteButton"  data-id="${doc.id}">Delete</button>
+                    <button class="editButton"  data-id="${doc.id}">Edit</button>
+				    </section>
+          </div>
+				       
+             `;
       });
+      postContainer.innerHTML = htmladd;
       /* likes */
       const buttonLike = postContainer.querySelectorAll('.likeButton');
       buttonLike.forEach((btn) => {
@@ -149,13 +91,33 @@ export const wall = () => {
           console.log(currentUserLike);
           const idLikeButton = event.target.dataset.id;
           console.log(idLikeButton);
-            likePost(currentUserLike, idLikeButton);
-          //  deletePost(currentUserLike, idLikeButton);
+          console.log('add');
+
+          likePost(currentUserLike, idLikeButton);
+        });
+      });
+      // Eliminar megusta
+      const buttonLikeremove = postContainer.querySelectorAll('.RemovelikeButton');
+      buttonLikeremove.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+          const currentUserLike = auth.currentUser.uid;
+          console.log('eliminando');
+          const idLikeButton = event.target.dataset.id;
+          console.log(idLikeButton);
+          DeletelikePost(currentUserLike, idLikeButton);
         });
       });
 
-      const buttonEdit = postContainer.querySelectorAll('.editButton');
+      // Eliminar
+      const buttonDelete = postContainer.querySelectorAll('.deleteButton');
+      buttonDelete.forEach((btn) => {
+        btn.addEventListener('click', ({ target: { dataset } }) => {
+          deletePost(dataset.id);
+        });
+      });
 
+      // editar
+      const buttonEdit = postContainer.querySelectorAll('.editButton');
       buttonEdit.forEach((btn) => {
         btn.addEventListener('click', async (e) => {
           const doc = await getPostOne(e.target.dataset.id);
@@ -163,16 +125,13 @@ export const wall = () => {
           formPost.post.value = postOne.description;
           editStatus = true;
           id = e.target.dataset.id;
-          formPost.btnPost.innerText = 'Update';
+          // formPost.btnPost.innerText = 'Update';
         });
       });
     });
   });
 
-
-
-	return sectionWall;
-
+ // salir
   logOutButton.addEventListener('click', () => {
     logOut();
     window.location.hash = '';
